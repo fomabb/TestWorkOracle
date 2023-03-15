7c48ffaf98849c4104316951496db5c0bac71c2f
Z-Tech <fomabb@yandex.by>
2023-03-15 15:50:13 +0300

    fix search

--- src/main/java/com/kirilyuk/test_work_oracle/dao/GoodsDAO.java
+++ src/main/java/com/kirilyuk/test_work_oracle/dao/GoodsDAO.java
@@ -11,10 +11,10 @@ import java.util.List;
 @Repository
 public interface GoodsDAO extends JpaRepository<Goods, Long> {
 
-    @Query("select g from Goods g where g.order=null ")
+    @Query("select g from Goods g where g.order=null")
     List<Goods> getAllGoods();
 
-    @Query("select g from Goods g where g.order!=null ")
+    @Query("select g from Goods g where g.order!=null")
     List<Goods> getAllGootsOrder();
 
     @Query(value = "select * from goods g where g.order_id=?1", nativeQuery = true)
@@ -30,6 +30,6 @@ public interface GoodsDAO extends JpaRepository<Goods, Long> {
     @Query(value = "select count(*) from goods g where g.order_id=?1", nativeQuery = true)
     int countGoods(@Param("id") Long id);
 
-    @Query("select g from Goods g where g.goodCode ilike %:text% and g.order=null")
+    @Query("select g from Goods g where g.goodCode like %:text% and g.order=null")
     List<Goods> search(@Param("text") String text);
 }
